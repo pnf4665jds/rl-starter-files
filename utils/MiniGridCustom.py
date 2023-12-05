a28d9cd3150549b5eae530372f27aede5cf8b1f1
Yen <pnf4665jds@gmail.com>
2023-12-05 15:53:21 +0800

    Fix

--- utils/MiniGridCustom.py
+++ utils/MiniGridCustom.py
@@ -23,7 +23,7 @@ gym.register(id='MyEnv-v0', entry_point='utils:EdgeEnv')
 class EdgeEnv(MiniGridEnv):
     def __init__(
         self,
-        size=20,
+        size=40,
         agent_start_pos=(5, 5),
         agent_start_dir=0,
         max_steps: int | None = None,
@@ -35,7 +35,7 @@ class EdgeEnv(MiniGridEnv):
         mission_space = MissionSpace(mission_func=self._gen_mission)
 
         if max_steps is None:
-            max_steps = size**2
+            max_steps = 300
 
         super().__init__(
             mission_space=mission_space,
@@ -43,7 +43,7 @@ class EdgeEnv(MiniGridEnv):
             # Set this to True for maximum speed
             see_through_walls=True,
             max_steps=max_steps,
-            agent_view_size=7,
+            agent_view_size=17,
             **kwargs,
         )
         self.action_space = spaces.Discrete(3)
@@ -53,8 +53,9 @@ class EdgeEnv(MiniGridEnv):
     def _gen_mission():
         return "grand mission"
 
-    def set_target(self, node_list_2d):
+    def set_target(self, node_list_2d, root_pos):
         self.node_list_2d = node_list_2d
+        self.root_pos = root_pos
 
     def _gen_grid(self, width, height):
         # Create an empty grid
@@ -64,12 +65,16 @@ class EdgeEnv(MiniGridEnv):
         self.grid.wall_rect(0, 0, width, height)
         
         if self.node_list_2d == None:
-            self.agent_dir = 0
-            self.agent_pos = (1, 1)
-            return
-
-        goal_i_list = []
-        goal_j_list = []
+            sample_points = [[2, 10], [26, 10], [14, 20]]
+            idx = random.randint(0, 2)
+            self.root_pos = sample_points[idx]
+            self.node_list_2d = []
+            self.node_list_2d.append([2, 15, 9])
+            self.node_list_2d.append([5, 15, 0])
+            self.node_list_2d.append([12, 15, 9])
+            self.node_list_2d.append([9, 15, 0])
+            self.node_list_2d.append([7, 10, 1])
+            self.node_list_2d.append([7, 8, 10])
 
         for i in range(1, height - 1):
             for j in range(1, width - 1):
@@ -77,29 +82,27 @@ class EdgeEnv(MiniGridEnv):
 
         for node in self.node_list_2d:
             # Place the goal
-            goal_i = 20-(round(node[0] * 2))
-            goal_j = round(node[1] * 2) + 10
+            goal_x = round(node[0] * 2)
+            goal_y = 40-(round(node[1] * 2))
             category = node[2]
             self.goal = Goal()
-            self.grid.set(goal_j, goal_i, self.goal)
-            goal_i_list.append(goal_i)
-            goal_j_list.append(goal_j)
-            
+            self.grid.set(goal_x, goal_y, self.goal)
+         
             # Generate wall
             for i in range(1, height - 1):
-                if i == goal_i or (category == 9) or self.grid.get(goal_j, i).type == 'goal':
+                if i == goal_y or (category == 9) or self.grid.get(goal_x, i).type == 'goal':
                     continue
-                self.grid.set(goal_j, i, Floor(color="grey"))
+                self.grid.set(goal_x, i, Floor(color="grey"))
             for j in range(1, width - 1):
-                if j == goal_j or (category == 10) or self.grid.get(j, goal_i).type == 'goal':
+                if j == goal_x or (category == 10) or self.grid.get(j, goal_y).type == 'goal':
                     continue
-                self.grid.set(j, goal_i, Floor(color="grey"))
+                self.grid.set(j, goal_y, Floor(color="grey"))
 
-        self.goal_count = len(goal_i_list)
+        self.goal_count = len(self.node_list_2d)
 
-        self.grid.set(goal_j_list[0], goal_i_list[0], None)
         self.agent_dir = 0
-        self.agent_pos = (goal_j_list[0], goal_i_list[0])
+        self.agent_pos = (self.root_pos[0], self.root_pos[1])
+        self.node_list_2d = None
 
     def step(self, action):
         self.step_count += 1
@@ -128,10 +131,9 @@ class EdgeEnv(MiniGridEnv):
         elif action == self.actions.forward:
             if fwd_cell is None or fwd_cell.can_overlap():
                 self.agent_pos = tuple(fwd_pos)
-                self.grid.set(fwd_pos[0], fwd_pos[1], Floor(color="yellow"))
+                self.grid.set(fwd_pos[0], fwd_pos[1], Floor(color="grey"))
             if fwd_cell is not None and fwd_cell.type == "goal":
-                self.grid.set(fwd_pos[0], fwd_pos[1], Floor(color="yellow"))
-                reward = 0.1
+                reward = self._reward()
                 self.goal_count -= 1
                 
         else:
@@ -145,7 +147,7 @@ class EdgeEnv(MiniGridEnv):
 
         if self.goal_count <= 0:
             terminated = True
-            reward = self._reward()
+            reward = 10
 
         obs = self.gen_obs()
 
